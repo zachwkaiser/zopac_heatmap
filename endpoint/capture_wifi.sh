722e9b306e2db421643ec827e669446bc5444f14
alexgromme <alexgromme@gmail.com>
2025-09-28 22:26:44 -0400

    Fixed typoes in the scripts that caused  errors. Relates to issue #9

--- endpoint/capture_wifi.sh
+++ endpoint/capture_wifi.sh
@@ -8,9 +8,9 @@ mkdir -p "$LOGDIR"
 timeStamp="$(date +%Y%m%d_%H%M%S)"
 outfile="$LOGDIR/captures_${IFACE}_${timeStamp}.log"
 
-sudo iw dev $IFACE set channel 1
+sudo iw dev $IFACE set channel 6
 
 echo "Starting tcpdump on $IFACE"
 echo "Writing to $outfile (CTRL + C to stop)"
 
-sudo tcpdump -i "$IFACE" -s 0 -l -e -tt -n -vvv >"$outfile"/
+sudo tcpdump -i "$IFACE" -s 0 -l -e -tt -n -vvv >"$outfile"
